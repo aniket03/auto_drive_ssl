0b87e3859fefb0f7e1ddafb4fad04325d653fcd1
Aniket <aniket.bhatnagar0310@gmail.com>
2020-04-29 11:37:50 -0400

    Lets unblock layers in phases

--- fcn_auto_train_test.py
+++ fcn_auto_train_test.py
@@ -27,11 +27,10 @@ if __name__ == '__main__':
                         help='input batch size for training (default: 32)')
     parser.add_argument('--epochs', type=int, default=150, metavar='N',
                         help='number of epochs to train (default: 150)')
-    parser.add_argument('--lr', type=float, default=1e-2, metavar='LR',
-                        help='learning rate (default: 0.01), since fine tunes from pre-text trained net')
     parser.add_argument('--weight-decay', type=float, default=5e-4,
                         help='Weight decay constant (default: 5e-4)')
     parser.add_argument('--tmax-for-cos-decay', type=int, default=50)
+    parser.add_argument('--early-stop-patience', type=int, default=7)
     parser.add_argument('--warm-start', type=bool, default=False)
     parser.add_argument('--only-train', type=bool, default=False,
                         help='If true utilize the annotated dataset for training.')
@@ -76,19 +75,15 @@ if __name__ == '__main__':
 
     # Train required model using data loaders defined above
     epochs = args.epochs
-    lr = args.lr
     weight_decay_const = args.weight_decay
 
     # Define model(s) to train
     aux_model = CombineAndUpSample(n_feature=64)
     main_model = fcn_resnet(args.model_type, 1)
 
-    # Set device on which training is done. Plus optimizer to use.
+    # Set device on which training is done.
     aux_model.to(device)
     main_model.to(device)
-    params = list(aux_model.parameters()) + list(main_model.parameters())
-    sgd_optimizer = optim.SGD(params, lr=lr, momentum=0.9, weight_decay=weight_decay_const)
-    scheduler = CosineAnnealingLR(sgd_optimizer, args.tmax_for_cos_decay, eta_min=1e-4, last_epoch=-1)
 
     # Inherit weights for aux model from pre-trained SSL aux model
     aux_file_path = os.path.join(PAR_WEIGHTS_DIR, args.ssl_trained_aux_file)
@@ -102,22 +97,77 @@ if __name__ == '__main__':
     weight_copy_success = copy_weights_between_models(simclr_model, main_model)
     del simclr_model
 
-    # Start training
-    model_train_test_obj = FCNModelTrainTest(
-        aux_model, main_model, device, model_file_path
-    )
-    train_losses, val_losses, train_accs, val_accs = [], [], [], []
-    for epoch_no in range(1, epochs + 1):
-        train_loss, train_acc, val_loss, val_acc = model_train_test_obj.train(
-            sgd_optimizer, epoch_no, params_max_norm=4,
-            train_data_loader=train_loader, val_data_loader=val_loader,
-            no_train_samples=len(train_set), no_val_samples=len(val_set)
-        )
-        train_losses.append(train_loss)
-        val_losses.append(val_loss)
-        train_accs.append(train_acc)
-        val_accs.append(val_acc)
-        scheduler.step()
+    # Freeze all layers in the aux model and main model's resnet module
+    for name, param in aux_model.named_parameters():
+        param.requires_grad = False
+    for name, param in main_model.named_parameters():
+        if name[:7] == 'resnet_':
+            param.requires_grad = False
 
-    # Log train-test results
-    log_experiment(args.experiment_name, args.epochs, train_losses, val_losses, train_accs, val_accs)
+    unblock_levels = [0, 1, 2, 3]  # 0=> block all, 1=> block till residual block 3, 2=> block till residual block 2
+                                   # 3 means unblock all
+    for unblock_level in unblock_levels:
+
+        if unblock_level == 0:
+            lr = 0.5
+
+        if unblock_level == 1:
+            lr = 0.1
+            for name, param in main_model.named_parameters():
+                if name[:15] == 'resnet_module.7':
+                    param.requires_grad = True
+
+        if unblock_level == 2:
+            lr = 0.05
+            for name, param in main_model.named_parameters():
+                if name[:15] == 'resnet_module.6' or name[:15] == 'resnet_module.7':
+                    param.requires_grad = True
+
+        else:
+            lr = 0.01
+            for name, param in aux_model.named_parameters():
+                param.requires_grad = True
+            for name, param in main_model.named_parameters():
+                param.requires_grad = True
+
+        # # To test what is trainable status of each layer
+        # for name, param in aux_model.named_parameters():
+        #     print (name, param.requires_grad)
+        # for name, param in main_model.named_parameters():
+        #     print (name, param.requires_grad)
+
+        # Start training
+        model_train_test_obj = FCNModelTrainTest(
+            aux_model, main_model, device, model_file_path
+        )
+        params = list(aux_model.parameters()) + list(main_model.parameters())
+        sgd_optimizer = optim.SGD(params, lr=lr, momentum=0.9, weight_decay=weight_decay_const)
+        scheduler = CosineAnnealingLR(sgd_optimizer, args.tmax_for_cos_decay, eta_min=1e-4, last_epoch=-1)
+        train_losses, val_losses, train_accs, val_accs = [], [], [], []
+        init_val_loss = 1e9
+        val_loss_threshold = 1e-4
+        early_stop_cnt = 0
+        for epoch_no in range(1, epochs + 1):
+            train_loss, train_acc, val_loss, val_acc = model_train_test_obj.train(
+                sgd_optimizer, epoch_no, params_max_norm=4,
+                train_data_loader=train_loader, val_data_loader=val_loader,
+                no_train_samples=len(train_set), no_val_samples=len(val_set)
+            )
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
+            train_accs.append(train_acc)
+            val_accs.append(val_acc)
+            scheduler.step()
+
+            if val_loss < init_val_loss - val_loss_threshold:
+                init_val_loss = val_loss
+                early_stop_cnt = 0
+            else:
+                early_stop_cnt += 1
+                if early_stop_cnt >= args.early_stop_patience:
+                    print ('Val loss not decreasing so doing early stop')
+                    break
+
+        # Log train-test results
+        log_experiment(args.experiment_name + '_ub_level_{}'.format(unblock_level),
+                       args.epochs, train_losses, val_losses, train_accs, val_accs)
