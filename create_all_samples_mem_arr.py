f058c1b352d8d6ee256979d936f5e0c9774030ce
Aniket <aniket.bhatnagar0310@gmail.com>
2020-05-02 23:55:25 -0400

    obtain embeddings using torch.no_grad and rectify error in file paths in main

--- create_all_samples_mem_arr.py
+++ create_all_samples_mem_arr.py
@@ -19,23 +19,24 @@ def get_all_samples_mem_arr(device, sample_data_loader, aux_model, main_model,
         batch_scene_indices = batch_scene_indices.to(device)
         batch_sample_indices = batch_sample_indices.to(device)
 
-        # Pass data_batch through the aux_model to get latent 800x800 representation
-        i_batch = aux_model(data_batch)
-        del data_batch
+        with torch.no_grad():
+            # Pass data_batch through the aux_model to get latent 800x800 representation
+            i_batch = aux_model(data_batch)
+            del data_batch
 
-        # Forward pass through the main network
-        vi_batch, vi_t_batch = main_model(i_batch, i_batch)
-        del i_batch, vi_t_batch
+            # Forward pass through the main network
+            vi_batch, vi_t_batch = main_model(i_batch, i_batch)
+            del i_batch, vi_t_batch
 
-        # Convert vi_batch to a numpy array
-        vi_batch = vi_batch.numpy()
-        if all_samples_mem is None:
-            all_samples_mem = vi_batch
-        else:
-            all_samples_mem = np.concatenate([all_samples_mem, vi_batch], axis=0)
+            # Convert vi_batch to a numpy array
+            vi_batch = vi_batch.numpy()
+            if all_samples_mem is None:
+                all_samples_mem = vi_batch
+            else:
+                all_samples_mem = np.concatenate([all_samples_mem, vi_batch], axis=0)
 
-        del batch_scene_indices, batch_sample_indices
-        del vi_batch
+            del batch_scene_indices, batch_sample_indices
+            del vi_batch
 
     np.save(all_samples_mem_file, all_samples_mem)
 
@@ -46,9 +47,9 @@ if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # Define model file_paths
-    aux_model_file = PAR_WEIGHTS_DIR + 'e1_simclr_auto_aux_epoch_30'
-    main_model_file = PAR_WEIGHTS_DIR + 'e1_simclr_auto_main_epoch_30'
-    all_samples_mem_file = PAR_ACTIVATIONS_DIR + 'e1_simclr_auto_activ_epoch_30.npy'
+    aux_model_file = PAR_WEIGHTS_DIR + '/e1_simclr_auto_aux_epoch_90'
+    main_model_file = PAR_WEIGHTS_DIR + '/e1_simclr_auto_main_epoch_90'
+    all_samples_mem_file = PAR_ACTIVATIONS_DIR + '/e1_simclr_auto_activ_epoch_90.npy'
 
     # Get the initial random weight models
     aux_model = CombineAndUpSample(n_feature=64)
